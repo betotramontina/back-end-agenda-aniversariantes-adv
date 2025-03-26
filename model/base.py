e8a7e451ebaa141453ddcef4a81655167f0bce84
betotramontina <122474917+betotramontina@users.noreply.github.com>
2025-03-26 11:48:01 -0300

    Create base.py

---
+++ model/base.py
@@ -0,0 +1,4 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+# # Define uma classe Base que será usada como ponto de partida para a definição de tabelas no banco de dados
+Base = declarative_base()
